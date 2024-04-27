b9b0b59c5e266b20323ed58665158d421fb6acf4
isabellkaitlyn <isabell.allard@gmail.com>
2024-04-26 23:48:08 -0700

    Start game

---
+++ Lighting and Post-Processing/Assets/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * 0.01f;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * 0.01f;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.01f;
+        }
+    }
+}
